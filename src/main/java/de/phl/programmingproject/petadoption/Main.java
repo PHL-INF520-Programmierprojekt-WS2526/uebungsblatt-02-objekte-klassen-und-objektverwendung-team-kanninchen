3f624c699df1cd60846cdc31f913fc431b01b093
Müjgan <macgin06@gmail.com>
2025-11-04 16:10:33 +0100

    Pet Adaptation erledigt

--- src/main/java/de/phl/programmingproject/petadoption/Main.java
+++ src/main/java/de/phl/programmingproject/petadoption/Main.java
@@ -2,11 +2,37 @@ package de.phl.programmingproject.petadoption;
 
 import java.util.Date;
 
-/**
- * PUT YOUR CODE FOR THE EXERCISE 'Pet Adoption Center' HERE.
- */
+
 public class Main {
     public static void main(String[] args) {
-        // TODO implement your program here
+        
+        Pet pet = new Pet("Buddy", 3, "Labrador Retriever");
+
+        
+        Adopter adopter = new Adopter("Jane Smith", "555-555-5555", "janesmith@email.com");
+
+        
+        System.out.println(" Haustierinformationen:");
+        System.out.println(pet.getInfo());
+
+        
+        System.out.println("Adoptierendeninformationen:");
+        System.out.println(adopter.getInfo());
+
+        
+        System.out.println("Wurde das Haustier adoptiert? " + pet.isAdopted());
+
+        
+        Adoption adoption = pet.adopt(adopter, new Date());
+
+        
+        System.out.println("Adoptionsinformationen:");
+        System.out.println(adoption.getInfo());
+
+        
+        System.out.println("Wurde das Haustier adoptiert? " + pet.isAdopted());
     }
 }
+
+    
+
